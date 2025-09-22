1a5f1ec538e15337ffe330fdfe92222f2b49ff50
qdft <qdftmods@gmail.com>
2025-09-22 22:56:28 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,1 @@
---if game.PlaceId == Game ID HERE then
---elseif game.PlaceId == Game ID HERE then
-else
 loadstring(game:HttpGet("https://raw.githubusercontent.com/qdftt/DMONRBX/refs/heads/main/Universal.lua"))()
-end
