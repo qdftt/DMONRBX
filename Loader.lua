70257bc2ce83bc26af2c8758a4e2c1fc0711f243
qdft <qdftmods@gmail.com>
2025-09-22 13:58:05 +0100

    Create Loader.lua

---
+++ Loader.lua
@@ -0,0 +1,5 @@
+--if game.PlaceId == Game ID HERE then
+--elseif game.PlaceId == Game ID HERE then
+else
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qdftt/DMONRBX/refs/heads/main/Universal.lua"))()
+end
