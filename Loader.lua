34fb1d57ecc1a17d727b5b3772aacdae804f5802
qdft <qdftmods@gmail.com>
2025-10-03 22:14:16 +0100

    Update and rename Loader.lua to loader.lua

--- Loader.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/qdftt/DMONRBX/refs/heads/main/Universal.lua"))()
---
+++ loader.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/qdftt/DMONRBX/refs/heads/main/Games/universal.lua"))()
