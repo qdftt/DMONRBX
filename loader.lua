785e12fd54c1f96c63f74ea07e3942b223491f6a
qdft <qdftmods@gmail.com>
2025-10-04 02:05:50 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/qdftt/DMONRBX/refs/heads/main/Games/universal.lua"))()
+local gameScripts = {
+    [3956818381] = "https://raw.githubusercontent.com/qdftt/DMONRBX/refs/heads/main/Games/ninja%20legends.lua"
+}
+
+local scriptUrl = gameScripts[game.PlaceId] or "https://raw.githubusercontent.com/qdftt/DMONRBX/refs/heads/main/Games/universal.lua"
+loadstring(game:HttpGet(scriptUrl))()
