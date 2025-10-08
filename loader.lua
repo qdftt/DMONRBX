39f41499d8f6a734a0eee75e107f9655dca6502c
qdft <qdftmods@gmail.com>
2025-10-08 20:05:39 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,6 @@
 local gameScripts = {
     [3956818381] = "https://raw.githubusercontent.com/qdftt/DMONRBX/refs/heads/main/Games/ninja%20legends.lua",
+    [3101667897] = "https://raw.githubusercontent.com/qdftt/DMONRBX/refs/heads/main/Games/legends%20of%20speed.lua",
 }
 
 local scriptUrl = gameScripts[game.PlaceId] or "https://raw.githubusercontent.com/qdftt/DMONRBX/refs/heads/main/Games/universal.lua"
